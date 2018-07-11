5fcf5fdb1a5cb3e57d95317ba256ba568aea2055
vinicius emanuel <vncemanuel@gmail.com>
2018-07-11 07:57:34 -0300

    rodei 4 versões do  Gradient_Boosting

--- gradientBoosting2.py
+++ gradientBoosting2.py
@@ -3,9 +3,12 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.svm import LinearSVC
 from sklearn.externals import joblib
+from imblearn.over_sampling import SMOTE, ADASYN
 
+import os.path as path
 import numpy as np
 import pandas as pd
+import timeit
 
 def gradientBoosting(state=42):
     gradientBoostingClassifier = GradientBoostingClassifier(
@@ -31,12 +34,9 @@ def gradientBoosting(state=42):
         warm_start=False, 
         presort='auto'
     )
-    return (gradientBoostingClassifier, "Gradient Boosting") 
+    return (gradientBoostingClassifier, "Gradient_Boosting_2") 
 
-def printar(classifier, answerAll=42, name = "Classificador"):
-    
-    print(f'rodado: {name}')
-
+def dividir(answerAll=42):
     np.random.seed(answerAll)
 
     data_set = pd.read_csv('data/TRN',sep='\t')
@@ -45,35 +45,119 @@ def printar(classifier, answerAll=42, name = "Classificador"):
     # Também convertemos os dados para arrays ao invés de DataFrames
     X = data_set.iloc[:, :-2].values
     y = data_set.iloc[:, -1].values
-
+ 
     # Treino: 50%, Validação: 25%, Teste: 25%
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/4, 
                                                         random_state=answerAll, stratify=y)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=1/3, 
-                                                    random_state=answerAll, stratify=y_train)
+    #X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=1/3, 
+    #                                                 random_state=answerAll, stratify=y_train)
 
-    fit = classifier.fit(X_train, y_train)
-    # joblib.dump(classifier, "gradientBoosting1.txt")
+    # train_test_split(y, shuffle=False)
 
+    X_resampled, y_resampled = SMOTE(kind='borderline1').fit_sample(X_train, y_train)
+
+    return (X_resampled, y_resampled, X_test, y_test)
+
+
+def metricas(classifier, X_test, y_test, name = "Classificador"):
+    
     score = classifier.score(X_test, y_test)
-    print(f'O Score do {name} é {score}')
-
     accuracy = cross_val_score(classifier, X_test, y_test, scoring='accuracy')
     average_precision = cross_val_score(classifier, X_test, y_test, scoring='average_precision')
     precision = cross_val_score(classifier, X_test, y_test, scoring='precision')
     recall = cross_val_score(classifier, X_test, y_test, scoring='recall')
     roc_auc = cross_val_score(classifier, X_test, y_test, scoring='roc_auc')
+  
+    score = f'Score: {score}'
+    accuracy = f'Accuracy: {accuracy}'
+    average_precision = f'average_precision: {average_precision}'
+    precision = f'precision: {precision}'
+    recall = f'recall: {recall}'
+    roc_auc = f'roc_auc: {roc_auc}'
 
-    print(f'Accuracy: {accuracy}')
-    print(f'average_precision: {average_precision}')
-    print(f'precision: {precision}')
-    print(f'recall: {recall}')
-    print(f'roc_auc: {roc_auc}')
+    return [score, accuracy, average_precision, precision, recall, roc_auc]
+
+
+def saveLog(classifier, metricas, file_name, time, name):
+    
+    name = "log/" + name + ".txt"
+    texto = []
+    try:
+        arquivo = open(name, 'r')
+        texto = arquivo.readlines()
+        arquivo = open(name, 'w')
+    except FileNotFoundError:
+        arquivo = open(name, 'w')
+   
+    texto.append("Rede treinada:\n")
+    texto.append(file_name)
+    texto.append("\n\nTempo de treinamento:\n")
+    texto.append("{0:.2f}s".format(time))
+    texto.append("\n\nConfiguração do classificador:\n")
+    texto.append(classifier.__str__())
+    texto.append("\n\nMetricas do classificador:\n")
+   
+    for metrica in metricas:
+        texto.append(metrica+"\n")
+
+    texto.append("---------------------------------------------------------\n\n")
+
+    # escrever no arquivo
+    arquivo.writelines(texto)
+    arquivo.close()
+
+def saveModel(classifier, name = "Classificador"):
+
+    name = "log/" + name
+    i = 0
+    while path.isfile(name + '_' + str(i) + '.pkl'):
+        i = i + 1
+
+    name_new = name + "_" + str(i) + ".pkl"
+    joblib.dump(classifier, name_new)
+    return name_new
 
 def main():
+
     answerAll = 42
-    classifier, name = gradientBoosting(answerAll)
-    printar(classifier, answerAll, name)
+    start = timeit.default_timer()
+
+    # divide os dados de entrada
+    X_train, y_train, X_test, y_test = dividir(answerAll)
+
+    loadModel = False
+
+    if loadModel:
+         # classificadores salvos
+        name = "Gradient_Boosting_2"
+        path = "log/Gradient_Boosting_2_0.pkl"
+        classifier = joblib.load(path) 
+
+        metricas_list = metricas(classifier, X_test, y_test, name)
+        print("Metricas do classificador: " + name)
+        for metrica in metricas_list:
+            print(metrica)
+
+        stop = timeit.default_timer()
+        print("Tempo de execução: {0:.2f}s".format(stop - start))
+    else:
+        classifier, name = gradientBoosting(answerAll)
+        
+        # treinar o modelo
+        classifier.fit(X_train, y_train)
+
+        # printar os resultados das metricas
+        metricas_list = metricas(classifier, X_test, y_test, name)
+        print("Metricas do classificador: " + name)
+        for metrica in metricas_list:
+            print(metrica)
+
+        name_model = saveModel(classifier, name)
+        stop = timeit.default_timer()
+        time = stop - start
+        print("Tempo de execução: {0:.2f}s".format(time))
+        saveLog(classifier, metricas_list, name_model, time, name)
+
 
 if __name__ == "__main__":
     main()
